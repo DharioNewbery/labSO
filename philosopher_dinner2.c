3db902c876cb8a0a427573fbfe0bee88f03c92ff
DharioNewbery <dharionew@gmail.com>
2026-06-14 22:56:13 -0300

    .

--- philosopher_dinner2.c
+++ philosopher_dinner2.c
@@ -5,8 +5,9 @@
 #include <sys/sem.h>
 
 #define N 5
+#define CICLES 3
 
-union semun { int val; };
+struct semun { int val; };
 
 void take(int sem, int f) {
     struct sembuf op = { f, -1, 0 };
@@ -23,7 +24,7 @@ void philosopher(int id, int sem) {
     int a = left < right ? left : right;
     int b = left < right ? right : left;
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < CICLES; i++) {
         printf("Philosopher %d (pid %d): thinking\n", id, getpid());
         sleep(1);
 
@@ -40,9 +41,30 @@ void philosopher(int id, int sem) {
     }
 }
 
-int main(void) {
+int get_semaphore_value(int semid, int sem_num) {
+    int semval = semctl(semid, sem_num, GETVAL);
+    
+    if (semval == -1) {
+        perror("semctl GETVAL failed");
+    }
+    return semval;
+}
+
+void printer(int sem) {
+    while (true) {
+        system("clear");
+        for (int i = 0; i < N; i++) {
+            printf("%d -> "get_semaphore_value(sem, i));
+        }
+        printf("\n");
+        usleep(50);
+    }
+}
+
+int main() {
     int sem = semget(IPC_PRIVATE, N, IPC_CREAT | 0666);
-    union semun val = { .val = 1 };
+    struct semun val = { 1 };
+
     for (int i = 0; i < N; i++)
         semctl(sem, i, SETVAL, val);
 
@@ -51,6 +73,11 @@ int main(void) {
             philosopher(i, sem);
             exit(0);
         }
+    }
+
+    if (fork() == 0) {
+        philosopher(i, sem);
+        exit(0);
     }
 
     for (int i = 0; i < N; i++)
